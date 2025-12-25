22d45a0cc3f4d3e116330aee6f8cf3f5737e041e
kunalSingh026 <skunalkumar759@gmail.com>
2025-12-25 12:18:53 +0530

    Updated Socket URL

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser && authUser._id) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://real-time-chatapp-shcn.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
